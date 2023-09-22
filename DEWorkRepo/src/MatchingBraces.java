1056d41239418416e65b2861934e1afaaea5cac7
Rory.McGuire25 <Rory.McGuire25@2024-313-2.bi.local>
2023-09-22 13:15:45 -0400

    Updated MatchingBraces

--- DEWorkRepo/src/MatchingBraces.java
+++ DEWorkRepo/src/MatchingBraces.java
@@ -1,4 +1,42 @@
 
-public class MatchingBraces {
 
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+
+public class MatchingBraces {
+	
+	
+	public static boolean scanFile(String filename) {
+		File f = null;
+		Scanner s = null;
+		f = new File(filename);
+		
+		try {
+			s = new Scanner(f);
+		} catch(FileNotFoundException e) {
+			System.out.println("File not Found")
+		}
+		
+		
+		int numBraces = 0;
+		
+		while(s.hasNext()) {
+			String next = s.next();
+			for(int i = 0; i < next.length(); i ++) {
+				if(next.charAt(i) == '(') {
+					numBraces ++;
+				} else if(next.charAt(i) == ')') {
+					if(numBraces > 0) {
+						numBraces --;
+					} else {
+						return false;
+					}
+				}
+			}
+		}
+		
+		return numBraces == 0;
+	}
 }
