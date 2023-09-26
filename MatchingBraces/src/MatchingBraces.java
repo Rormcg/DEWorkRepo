649f4b70beff57f77659c04923efd64145699451
Rory McGuire <rory.mcguire25@bishopireton.org>
2023-09-26 18:58:54 -0400

    Added Javadocs to MatchingBraces

--- MatchingBraces/src/MatchingBraces.java
+++ MatchingBraces/src/MatchingBraces.java
@@ -1,4 +1,5 @@
 /**
+ * Class that will scan a file to ensure that all of its braces are matching
  * @author Rory McGuire
  */
 
@@ -8,19 +9,27 @@ import java.io.File;
 
 
 public class MatchingBraces {
+	public final static String SEP = File.separator;
 	
 	public static void main(String[] args) {
-		String filename = ".\\src\\test.txt";
+		String filename = "." + SEP + "src" + SEP + "test.txt";
 		
 		boolean isMatching = scanFile(filename);
 		System.out.println(filename + " " + isMatching);
 	}
 	
+	/**
+	 * Scans the given file to determine whether all braces within the file are paired up and
+	 * returns a boolean indicating the result
+	 * @param filename the exact or relative path to the file to be scanned
+	 * @return true if all braces in the file match up, false otherwise
+	 */
 	public static boolean scanFile(String filename) {
 		//System.out.println(System.getProperty("user.dir")); 
 		File f = new File(filename);
 		Scanner s = null;
-				
+		
+		//Initialize Scanner
 		try {
 			s = new Scanner(f);
 		} catch(FileNotFoundException e) {
@@ -28,9 +37,8 @@ public class MatchingBraces {
 			System.exit(0);
 		}
 		
-		
+		//Scan the file for pairs of braces
 		int numBraces = 0;
-		
 		while(s.hasNext()) {
 			String next = s.next();
 			for(int i = 0; i < next.length(); i ++) {
