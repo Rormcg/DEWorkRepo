97c849dca5f98e1726bfe7bbe6488544062a8487
Rory McGuire <rory.mcguire25@bishopireton.org>
2023-09-26 18:43:11 -0400

    Fixed filename error

--- MatchingBraces/src/MatchingBraces.java
+++ MatchingBraces/src/MatchingBraces.java
@@ -10,13 +10,14 @@ import java.io.File;
 public class MatchingBraces {
 	
 	public static void main(String[] args) {
-		String filename = "/MatchingBraces/src/test.txt";
+		String filename = ".\\src\\test.txt";
 		
 		boolean isMatching = scanFile(filename);
-		System.out.println(filename + isMatching);
+		System.out.println(filename + " " + isMatching);
 	}
 	
 	public static boolean scanFile(String filename) {
+		//System.out.println(System.getProperty("user.dir")); 
 		File f = new File(filename);
 		Scanner s = null;
 				
@@ -33,9 +34,9 @@ public class MatchingBraces {
 		while(s.hasNext()) {
 			String next = s.next();
 			for(int i = 0; i < next.length(); i ++) {
-				if(next.charAt(i) == '(') {
+				if(next.charAt(i) == '{') {
 					numBraces ++;
-				} else if(next.charAt(i) == ')') {
+				} else if(next.charAt(i) == '}') {
 					if(numBraces > 0) {
 						numBraces --;
 					} else {
