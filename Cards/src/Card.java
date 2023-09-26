a5c3bdff505d152f231c465eaf0fc8c2ddb67bc5
Rory McGuire <rory.mcguire25@bishopireton.org>
2023-09-26 19:17:08 -0400

    Created Cards Project

---
+++ Cards/src/Card.java
@@ -0,0 +1,15 @@
+/**
+ * Represents a playing card
+ * @author Rory McGuire
+ */
+
+public class Card {
+	private String suit;
+	private int rank;
+	
+	Card(String s, int r) {
+		suit = s;
+		rank = r;
+	}
+	
+}
---
+++ Cards/src/Deck.java
@@ -0,0 +1,13 @@
+/**
+ * Represents a deck of cards
+ * Contains an Array of Card objects
+ * @author Rory McGuire
+ */
+
+public class Deck {
+	private Card[] deck;
+	
+	Deck() {
+		deck = null;
+	}
+}
