1bc5f9c2cc60422e8e0095511642e301300ddade
Rory.McGuire25 <Rory.McGuire25@2024-313-4.bi.local>
2023-11-07 13:00:51 -0500

    Add and Begin Browsing Lab (Ch 22)

---
+++ Browsing/src/BrowserModel.java
@@ -0,0 +1,38 @@
+/**
+ * @author Rory McGuire
+ */
+
+
+public class BrowserModel {
+	//private Stack<String> = ;
+	private BrowserView view;
+	
+	public BrowserModel(BrowserView view) {
+		this.view = view;
+		//this.view.update();
+	}
+	
+	public void back() {
+		
+	}
+	
+	public void forward() {
+		
+	}
+	
+	public void followLink(int n) {
+		
+	}
+	
+	public void home() {
+		
+	}
+	
+	public boolean hasBack() {
+		return false;
+	}
+	
+	public boolean hasForward() {
+		return false;
+	}
+}
