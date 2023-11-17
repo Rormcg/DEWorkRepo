dce8a4687857dd77b6cf02fc37cb4ac201f7473f
Rory McGuire <rory.mcguire25@bishopireton.org>
2023-11-17 11:55:29 -0500

    Add files for graphics

---
+++ ProductionLine/src/InputLine.java
@@ -0,0 +1,15 @@
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class InputLine extends LinkedList<Disk> {
+
+	public InputLine() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InputLine(Collection c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ProductionLine/src/OutputLine.java
@@ -0,0 +1,15 @@
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class OutputLine extends LinkedList<Tower> {
+
+	public OutputLine() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public OutputLine(Collection c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+
+}
