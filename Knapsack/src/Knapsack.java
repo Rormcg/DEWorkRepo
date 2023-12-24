d1b17b8574c8ee536d1ffe86056d4c7acf1df8c2
Rory McGuire <rory.mcguire25@bishopireton.org>
2023-12-24 15:14:30 -0500

    Create Knapsack Project

---
+++ Knapsack/src/Knapsack.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+/**
+ * 
+ * @author Rory McGuire
+ */
+
+public class Knapsack {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
