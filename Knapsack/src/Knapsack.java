ab00a3138f4de4ade40571ecf1d062538d7e10df
Rory.McGuire25 <Rory.McGuire25@2024-313-4.bi.local>
2024-01-03 12:55:28 -0500

    Finished simple implementation of knapsackSum

--- Knapsack/src/Knapsack.java
+++ Knapsack/src/Knapsack.java
@@ -1,5 +1,7 @@
+import java.util.List;
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -8,9 +10,39 @@ import java.io.IOException;
  */
 
 public class Knapsack {
-
+	
 	public static void main(String[] args) {
+		String filename = "";
+		File f = new File(filename);
+		try {
+			Scanner s = new Scanner(f);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		//int[] numbers = new int[];
 		
 	}
-
+	
+	public static int knapsackSum(int[] w, int n, int limit) {
+		if(limit < 0) return 0;
+		if(n == 1) {
+			return w[n-1] < limit ? w[n-1] : 0;
+		}
+		
+		int valueWith = knapsackSum(w, n - 1, limit - w[n - 1]);
+		int valueWithout = knapsackSum(w, n - 1, limit);
+		
+		if(valueWith > valueWithout) {
+			//list.add(w[n-1]);
+			return valueWith + w[n-1];
+		} else {
+			return valueWithout;
+		}
+	}
+	
+	public static int knapsackSum(int[] w, int n, int limit, List<Integer> list) {
+		
+		return 0;
+	}
 }
