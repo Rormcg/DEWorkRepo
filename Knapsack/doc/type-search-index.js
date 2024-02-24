c1fddef286bcefcab28959a6993be37e24a5170e
Rory McGuire <rory.mcguire25@bishopireton.org>
2024-02-23 23:23:30 -0500

    Resolve Conflicts

--- Knapsack/doc/type-search-index.js
+++ Knapsack/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Knapsack"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Knapsack"},{"p":"<Unnamed>","l":"TestKnapsack"}];updateSearchResults();
