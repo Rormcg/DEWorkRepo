59a2558ec0848dbb753045f18289d0437e71f2fc
Rory.McGuire25 <Rory.McGuire25@2024-313-4.bi.local>
2024-04-12 12:55:15 -0400

    Add SimulatedMessages class

---
+++ PriorityMessageQueue/src/SimulatedMessages.java
@@ -0,0 +1,14 @@
+/**
+ * @author Rory McGuire
+ */
+public class SimulatedMessages {
+
+	/**
+	 * Run a simulated transmission of messages to a receiving MessagePriorityQueue
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
