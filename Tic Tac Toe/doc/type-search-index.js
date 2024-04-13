4fc29cc6245886dcecf242f765a61aaa002a014a
Rory McGuire <rory.mcguire25@bishopireton.org>
2024-04-13 13:27:13 -0400

    Generate TTT Part B Javadocs

--- Tic Tac Toe/doc/type-search-index.js
+++ Tic Tac Toe/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"TicTacToe"},{"p":"<Unnamed>","l":"TicTacToeHashCode"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"TicTacToe"},{"p":"<Unnamed>","l":"TicTacToeHashCode"},{"p":"<Unnamed>","l":"TTT_HC"}];updateSearchResults();
