28ece9a05899920f4d216b441ded16e2b12e5a71
Rory McGuire <rory.mcguire25@bishopireton.org>
2024-05-15 17:51:36 -0400

    Generate EightQueens Javadocs

---
+++ EightQueens/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ChessBoard"},{"p":"<Unnamed>","l":"ChessSquarePanel"},{"p":"<Unnamed>","l":"EightQueens"}];updateSearchResults();
